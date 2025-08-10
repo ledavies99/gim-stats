da39e9a04222ae2a08be99044c8cf06d6c726436
Luke Davies <lukedavies99@gmail.com>
2025-08-10 11:14:34 +0100

    Add routing for static files in debug mode

--- gim_project/urls.py
+++ gim_project/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 # gim_project/urls.py
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include # Import include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('stats_app.urls')), # Add this line
+    path('', include('stats_app.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    

7b8040719589be9066839d51e160f209c20cdab2
Luke Davies <lukedavies99@gmail.com>
2025-09-10 11:11:47 +0100

    Update react API calls

---
+++ frontend/src/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+export const getData = async () => {
+  const response = await axios.get(`${API_BASE_URL}your-endpoint/`);
+  return response.data;
+};
